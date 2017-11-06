68bf2563c5b0c2f2b499e9897a2538c4d01c7b7d
pituganov <pituganovstas@gmail.com>
2017-11-06 14:03:08 +0500

    Added ADD_PLATE & REMOVE_PLATE actions

--- src/Reducers/cart.js
+++ src/Reducers/cart.js
@@ -4,13 +4,34 @@ const initialState = [];
 
 export default function cart(state = initialState, action) {
     if (action.type === 'ADD_PLATE') {
-        return [
-            ...state,
-            action.payload
-        ]
+        var isNewPlate = true;
+        var i = 0;
+        while (isNewPlate && i < state.length) {
+            let equalTitle = action.payload.title === state[i].plate.title;
+            let equalRestaurant = action.payload.restourant === state[i].plate.restourant;
+            isNewPlate = !(equalTitle && equalRestaurant);
+            i++;
+        }
+
+        if (isNewPlate) {
+            return [
+                ...state,{
+                    plate: action.payload,
+                    count: 1
+                }
+            ];
+        }
+        else {
+            state[i-1].count++;
+            return state;
+        }
+        console.log(state);
     }
     if (action.type === 'REMOVE_PLATE') {
-        state.splice(action.index, 1);
+        if (state[action.index].count <= 1)
+            state.splice(action.index, 1);
+        else
+            state[action.index].count--;
         return state;
     }
     return state;
