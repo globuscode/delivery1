07383a029dd3c1fc32e68b179c4101e3cbbce7ce
pituganov <pituganovstas@gmail.com>
2017-11-27 12:08:54 +0500

    Decreased recomendation to 7

--- src/Reducers/recomendations.js
+++ src/Reducers/recomendations.js
@@ -9,6 +9,13 @@ const initialState = {
 
 export default function recomendations(state = initialState, action) {
   if (action.type == 'SET_RECOMENDATIONS') {
+      var d = action.payload.plates.sort( function() { return 0.5 - Math.random() } );
+      action.payload.plates = [];
+      const len = d.length < 7 ? d.length : 7;
+      for (let i=0; i < len; i++) {
+        action.payload.plates.push(d[i]);
+      }
+      //action.payload.plates = [d[0], d[1], d[2], d[3], d[4], d[5], d[6] ];
       return {
           ...state,
           ...action.payload
