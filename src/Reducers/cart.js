1c806238399553b06769b6e28954f2b66afe3b9f
pituganov <pituganovstas@gmail.com>
2017-11-27 12:08:12 +0500

    Added dispatch by object

--- src/Reducers/cart.js
+++ src/Reducers/cart.js
@@ -26,6 +26,11 @@ export default function cart(state = initialState, action) {
             let equalRestaurant = action.payload.restourant === state[i].plate.restourant;
             isNewPlate = !(equalTitle && equalRestaurant);
             i++;
+            if (!equalRestaurant)
+                return [{
+                    plate: action.payload,
+                    count: 1
+                }];
         }
 
         if (isNewPlate) {
@@ -46,6 +51,23 @@ export default function cart(state = initialState, action) {
             state.splice(action.index, 1);
         else
             state[action.index].count--;
+        return state;
+    }
+
+    if (action.type === 'REMOVE_PLATE_BY_OBJECT') {
+        //console.log("Цикл начат");
+        for (let index = 0; index < state.length; index++) {
+            //console.log(state[index].plate.id, action.payload.id);
+            if (action.payload.id == state[index].plate.id) {
+                console.log(state[index]);
+                if (state[index].count <= 1)
+                    state.splice(action.index, 1);
+                else
+                    state[index].count--;
+                break;
+            }
+        }
+        //console.log("Цикл закончен");
         return state;
     }
     return state;
