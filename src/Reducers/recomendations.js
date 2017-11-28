bb5e5e26281f6aa3e7fb836f46e50c58614f79c7
pituganov <pituganovstas@gmail.com>
2017-11-28 10:37:11 +0500

    decreased ammoun of restorans

--- src/Reducers/recomendations.js
+++ src/Reducers/recomendations.js
@@ -11,9 +11,16 @@ export default function recomendations(state = initialState, action) {
   if (action.type == 'SET_RECOMENDATIONS') {
       var d = action.payload.plates.sort( function() { return 0.5 - Math.random() } );
       action.payload.plates = [];
-      const len = d.length < 7 ? d.length : 7;
+      let len = d.length < 7 ? d.length : 7;
       for (let i=0; i < len; i++) {
         action.payload.plates.push(d[i]);
+      }
+
+      d = action.payload.restaurants.sort( function() { return 0.5 - Math.random() } );
+      action.payload.restaurants = [];
+      len = d.length < 7 ? d.length : 7;
+      for (let i=0; i < len; i++) {
+        action.payload.restaurants.push(d[i]);
       }
       //action.payload.plates = [d[0], d[1], d[2], d[3], d[4], d[5], d[6] ];
       return {
