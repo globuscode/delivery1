b631aaadf31a0d86f859869ac79cbb4d27adabe7
pituganov <pituganovstas@gmail.com>
2017-12-11 15:55:23 +0500

    Added adapt function

---
+++ src/etc/index.js
@@ -0,0 +1,14 @@
+import { Dimensions } from 'react-native';
+
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+
+const adaptWidth = (width320, width375, width414) => {
+    if (viewportWidth <= 320)
+        return width320;
+    if (320 < viewportWidth && viewportWidth <= 375)
+        return width375;
+    if (viewportWidth >= 414)
+        return width414;
+};
+
+export { adaptWidth };
