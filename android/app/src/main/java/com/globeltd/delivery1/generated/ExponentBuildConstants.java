847b347f03bc63e179f596b1bb33522635d835d2
pituganov <pituganovstas@gmail.com>
2018-01-09 10:57:29 +0500

    detach

---
+++ android/app/src/main/java/com/globeltd/delivery1/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.globeltd.delivery1.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
