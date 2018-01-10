7d9da1e14c9d84dada72fa77df3df33cf42ca3a3
pituganov <pituganovstas@gmail.com>
2018-01-10 12:56:56 +0500

    android config

--- android/app/src/main/java/com/globeltd/delivery1/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/globeltd/delivery1/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.globeltd.delivery1.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp78b044f144244ab3b9155c78dfdba98d://localhost:80";
 
 }
