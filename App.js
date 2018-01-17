6ab8c62e17583b1e51e7583a1d8fef4d45bdf9e8
pituganov <pituganovstas@gmail.com>
2018-01-17 11:14:24 +0500

    color of overlay for popup changed

--- App.js
+++ App.js
@@ -1,49 +1,54 @@
 import React from "react";
 import { Font } from "expo";
-import Expo from "expo";
-import { StatusBar, Platform, View, AsyncStorage } from "react-native";
-import { StackNavigator } from "react-navigation";
+import { StatusBar, View, AsyncStorage } from "react-native";
 import { Provider, connect } from "react-redux";
 
-import IconD from "./src/IconD";
 import Reducer from "./src/Reducers";
 import A from "./ScreenReducer";
 import PopupCart from "./src/PopupCart";
 
-import PopupDialog, {
-  SlideAnimation
-} from 'react-native-popup-dialog';
+import PopupDialog, { SlideAnimation } from "react-native-popup-dialog";
 
 StatusBar.setBarStyle("light-content", true);
 
 const slide = new SlideAnimation({
-  slideFrom: 'bottom',
+  slideFrom: "bottom"
 });
 
 class ModalComponent extends React.Component {
-  componentWillReceiveProps = (nextProps) => {this.setState({})}
-  render = () => <View style={{flex: 1}}>
-    <A />
-    <PopupDialog
-      dismissOnHardwareBackPress={true}
-      onDismissed={() => { Reducer.dispatch({type: "HIDE_MODAL"}) }}
-      ref={(popupDialog) => { this.popupDialog = popupDialog; }}
-      dismissOnTouchOutside={true}
-      dialogStyle={{
-        backgroundColor: 'transparent',
-        flexDirection: 'column',
-        flex: 1,
-        justifyContent: 'flex-end'
-      }}
-      containerStyle={{
-        justifyContent: 'flex-end',
-      }}
-      show={Reducer.getState().modalController.opened}
-      dialogAnimation={slide}
-    >
-    <PopupCart />
-  </PopupDialog>
-</View>
+  componentWillReceiveProps = () => {
+    //this.setState({});
+  };
+  render = () => (
+    <View style={{ flex: 1 }}>
+      <A />
+      <PopupDialog
+        dismissOnHardwareBackPress={true}
+        overlayBackgroundColor="rgb(37, 38, 46)"
+        overlayOpacity={0.9}
+        onDismissed={() => {
+          Reducer.dispatch({ type: "HIDE_MODAL" });
+        }}
+        ref={popupDialog => {
+          this.popupDialog = popupDialog;
+        }}
+        dismissOnTouchOutside={true}
+        dialogStyle={{
+          backgroundColor: "transparent",
+          flexDirection: "column",
+          flex: 1,
+          justifyContent: "flex-end"
+        }}
+        containerStyle={{
+          justifyContent: "flex-end"
+        }}
+        show={Reducer.getState().modalController.opened}
+        dialogAnimation={slide}
+      >
+        <PopupCart />
+      </PopupDialog>
+    </View>
+  );
 }
 
 const M = connect(
@@ -51,8 +56,8 @@ const M = connect(
     modal: state.modalController
   }),
   dispatch => ({
-    open: () => dispatch({ type: "OPEN_MODAL"}),
-    close: () => dispatch({ type: "CLOSE_MODAL"}),
+    open: () => dispatch({ type: "OPEN_MODAL" }),
+    close: () => dispatch({ type: "CLOSE_MODAL" })
   })
 )(ModalComponent);
 
@@ -69,20 +74,19 @@ export default class App extends React.Component {
       "stem-regular": require("./assets/fonts/Stem-Regular.ttf")
     });
 
-    const fav = await AsyncStorage.getItem('fav');
-    const nan = await AsyncStorage.getItem('nan');
+    const fav = await AsyncStorage.getItem("fav");
+    const nan = await AsyncStorage.getItem("nan");
     if (fav != nan)
-      Reducer.dispatch({type: 'SET_FAV', payload: JSON.parse(fav)});
+      Reducer.dispatch({ type: "SET_FAV", payload: JSON.parse(fav) });
 
     this.setState({ canRender: true });
   }
 
   render() {
-
     if (this.state.canRender)
       return (
         <Provider store={Reducer}>
-          <M/>
+          <M />
         </Provider>
       );
     else return null;
