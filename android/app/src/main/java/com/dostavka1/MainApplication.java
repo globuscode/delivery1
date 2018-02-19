333469c7401be72da4e5b0381c06fe98df974a84
pituganov <pituganovstas@gmail.com>
2018-02-19 08:38:50 +0500

    vk login unlinked

--- android/app/src/main/java/com/dostavka1/MainApplication.java
+++ android/app/src/main/java/com/dostavka1/MainApplication.java
@@ -3,7 +3,6 @@ package com.dostavka1;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import camp.kuznetsov.rn.vkontakte.VKAuthPackage;
 import com.zyu.ReactNativeWheelPickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VKAuthPackage(),
             new ReactNativeWheelPickerPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage()
--- ios/dostavka1/AppDelegate.m
+++ ios/dostavka1/AppDelegate.m
@@ -9,12 +9,6 @@
 
 #import "AppDelegate.h"
 
-#if __has_include(<VKSdkFramework/VKSdkFramework.h>)
-#import <VKSdkFramework/VKSdkFramework.h>
-#else
-#import "VKSdk.h"
-#endif
-
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -41,18 +35,6 @@
   return YES;
 }
 
-//iOS 9 workflow
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
-    [VKSdk processOpenURL:url fromApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]];
-    return YES;
-}
-
-//iOS 8 and lower
--(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
-{
-    [VKSdk processOpenURL:url fromApplication:sourceApplication];
-    return YES;
-}
 
 
 @end
