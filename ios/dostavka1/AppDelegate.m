80ca72a1c0eadb2de5052f32dd92f61ccfa52549
pituganov <pituganovstas@gmail.com>
2018-02-19 10:11:43 +0500

    vk-login installed

--- ios/dostavka1/AppDelegate.m
+++ ios/dostavka1/AppDelegate.m
@@ -9,6 +9,13 @@
 
 #import "AppDelegate.h"
 
+#if __has_include(<VKSdkFramework/VKSdkFramework.h>)
+#import <VKSdkFramework/VKSdkFramework.h>
+#else
+#import "VKSdk.h"
+#endif
+
+
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -34,6 +41,20 @@
   [self.window makeKeyAndVisible];
   return YES;
 }
+
+//iOS 9 workflow
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+    [VKSdk processOpenURL:url fromApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]];
+    return YES;
+}
+
+//iOS 8 and lower
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    [VKSdk processOpenURL:url fromApplication:sourceApplication];
+    return YES;
+}
+
 
 
 
