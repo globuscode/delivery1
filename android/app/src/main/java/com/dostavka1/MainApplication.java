3f241b2344ea21dc533d45e88f2c77b022a8e13a
pituganov <pituganovstas@gmail.com>
2018-02-22 10:35:09 +0500

    android link

--- android/app/src/main/java/com/dostavka1/MainApplication.java
+++ android/app/src/main/java/com/dostavka1/MainApplication.java
@@ -3,6 +3,10 @@ package com.dostavka1;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.centaurwarchief.smslistener.SmsListenerPackage;
 import camp.kuznetsov.rn.vkontakte.VKAuthPackage;
 import com.zyu.ReactNativeWheelPickerPackage;
@@ -24,10 +28,18 @@ public class MainApplication extends Application implements ReactApplication {
       return BuildConfig.DEBUG;
     }
 
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+    protected static CallbackManager getCallbackManager() {
+      return mCallbackManager;
+    }
+
     @Override
     protected List<ReactPackage> getPackages() {
+      
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new SmsListenerPackage(),
             new VKAuthPackage(),
             new ReactNativeWheelPickerPackage(),
@@ -50,6 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
