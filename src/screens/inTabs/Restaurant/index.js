bef91c49d6d68c3b2bd44613657bb141036122b8
pituganov <pituganovstas@gmail.com>
2018-03-19 17:37:12 +0500

    underfined rendering fixed

--- src/screens/inTabs/Restaurant/index.js
+++ src/screens/inTabs/Restaurant/index.js
@@ -374,36 +374,38 @@ class Restaurant extends React.Component {
           })}
 
           {/* Минимальная сумма заказа и бесплатная доставка */}
-          {this.state.data.freeDelivery == null ? null : (
-            <View
-              style={[
-                {
-                  justifyContent: "center",
-                  flexDirection: "row",
-                  left: -15,
-                  marginBottom: 38
-                }
-              ]}
-            >
-              <View style={{ width: 35, top: -2 }}>
-                <IconD color="#dcc49c" size={35} name="truck" />
-              </View>
-              <Text
-                style={{
-                  color: "#ffffff",
-                  fontSize: 11,
-                  fontFamily: "OpenSans",
-                  lineHeight: 13,
-                  top: 2,
-                  marginLeft: 15
-                }}
-              >{`Минимальная сумма заказа ${
-                  this.state.data.minBill
-                } ₽ \nБесплатная доставка от ${
-                  this.state.data.freeDelivery
-                } ₽`}</Text>
+
+          <View
+            style={[
+              {
+                justifyContent: "center",
+                flexDirection: "row",
+                left: -15,
+                marginBottom: 38
+              }
+            ]}
+          >
+            <View style={{ width: 35, top: -2 }}>
+              <IconD color="#dcc49c" size={35} name="truck" />
             </View>
-          )}
+            <Text
+              style={{
+                color: "#ffffff",
+                fontSize: 11,
+                fontFamily: "OpenSans",
+                lineHeight: 13,
+                top: 2,
+                marginLeft: 15
+              }}
+            >
+              {`Минимальная сумма заказа ${this.state.data.minBill} ₽ ` +
+                (this.state.data.freeDelivery == null
+                  ? ""
+                  : `\nБесплатная доставка от ${
+                    this.state.data.freeDelivery
+                  } ₽`)}
+            </Text>
+          </View>
 
           {/* Описание ресторана */}
           {this.state.data.description.description == "" &&
